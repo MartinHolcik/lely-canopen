6fcddc67f810671af91e05efdae533305008a69e
J. S. Seldenthuis <jseldenthuis@lely.com>
2023-01-12 11:38:46 +0100

    release v2.1.10

--- python/can/lely_can/__init__.py
+++ python/can/lely_can/__init__.py
@@ -1,8 +1,8 @@
 """Python bindings for the Lely CAN library."""
 
-__copyright__ = '2020 Lely Industries N.V.'
+__copyright__ = '2023 Lely Industries N.V.'
 __author__ = 'J. S. Seldenthuis <jseldenthuis@lely.com>'
-__version__ = '2.1.9'
+__version__ = '2.1.10'
 __license__ = 'Apache-2.0'
 
 from lely_can.err import *
